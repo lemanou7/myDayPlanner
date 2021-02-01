63d4d5ad54e766abc09ad7a973d24f7ffaee8403
Drissa Bagate <lemanou7@yahoo.fr>
2021-01-31 21:44:47 -0700

    fixing some styles

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
       // this should only occur on first time the app is loaded in the browser
       // helpfully remind user that lunch is important
       planTextArr = new Array(9);
-      planTextArr[4] = "Picnic lunch outside";
+      planTextArr[0] = "BBQ AT CENTRAL PARK NEW YORK";
     }
   
     // set variable referencing planner element
